5f83ecf2cb553ae1498517279cd9ab79f594fa28
Sebastian Petersen <sebastian@srmail.dk>
2018-09-16 13:20:24 +0200

    New ClassLib. New "Bil" class

---
+++ BilletLibrary/Bil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BilletLibrary
+{
+    public class Bil
+    {
+        public string NummerPlade { get; set; }
+        public DateTime Dato { get; set; }
+
+        public decimal Pris()
+        {
+            return 240;
+        }
+
+        public string Køretøj()
+        {
+            return "Bil";
+        }
+    }
+}
