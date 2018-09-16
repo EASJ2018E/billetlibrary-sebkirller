36756bce498c02a86116ba91eb8f32007220fc80
Sebastian Petersen <sebastian@srmail.dk>
2018-09-16 14:04:00 +0200

    Added check for licence plate.

--- BilletLibrary/Bil.cs
+++ BilletLibrary/Bil.cs
@@ -4,6 +4,17 @@ namespace BilletLibrary
 {
     public class Bil : Vehicle
     {
+        private string _nummerPlade;
+
+        private string NummerPlade
+        {
+            get => _nummerPlade;
+            set => _nummerPlade = value.Length <= 7 ? value
+                : throw new ArgumentException("Nummerpladen må ikke være længere end 7 tegn!");
+        }
+        
+        private DateTime Dato { get; set; }
+        
         public override decimal Pris()
         {
             return 240;
--- BilletLibrary/Vehicle.cs
+++ BilletLibrary/Vehicle.cs
@@ -4,9 +4,6 @@ namespace BilletLibrary
 {
     public abstract class Vehicle
     {
-        private string NummerPlade { get; set; }
-        private DateTime Dato { get; set; }
-
         public abstract decimal Pris();
 
         public abstract string Køretøj();
